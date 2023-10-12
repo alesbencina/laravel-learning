f04771f4ac08ae11dbb94f22601c39a617be0626
Ales Bencina <ales.bencina@agiledrop.com>
2023-10-12 15:36:58 +0200

    Add SEO important fields

--- resources/views/livewire/frontend/blog/detail.blade.php
+++ resources/views/livewire/frontend/blog/detail.blade.php
@@ -1,3 +1,6 @@
+@section('meta_title', $post->title . ' - AB Blog')
+@section('meta_description', $post->summary)
+
 <div>
     <article class="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <section class="flex items-center gap-4">
@@ -35,4 +38,56 @@
 
     </article>
     <script>hljs.highlightAll();</script>
+    <script type="application/ld+json">
+    {
+        "@context": "https://schema.org/",
+        "@type": "Blog",
+        "@id": "https://dataliberate.com/blog/",
+        "mainEntityOfPage": "https://dataliberate.com/blog",
+        "name": "Ales Bencina Blog",
+        "description": "Thoughts about technologies that are happening in my career path",
+        "publisher": {
+            "@type": "Organization",
+            "@id": "https://dataliberate.com",
+            "name": "Ales Bencina",
+            "logo": {
+                "@type": "ImageObject",
+                "@id": "https://dataliberate.com/wp-content/uploads/2011/12/Data_Liberate_Logo-200.png",
+                "url": "https://dataliberate.com/wp-content/uploads/2011/12/Data_Liberate_Logo-200.png",
+                "width": "600",
+                "height": "60"
+            }
+        },
+        "blogPost": [
+            {
+                "@type": "BlogPosting",
+                "@id": "https://dataliberate.com/2019/05/14/library-metadata-evolution-final-mile/#BlogPosting",
+                "mainEntityOfPage": "{{ $post->title }}",
+                "headline": "{{ $post->title }}",
+                "name": "{{ $post->title }}",
+                "description": "{{ $post->summary }}",
+                "datePublished": "2019-05-14",
+                "dateModified": "2019-05-14",
+                "author": {
+                    "@type": "Person",
+                    "@id": "https://dataliberate.com/author/richard-wallis/#Person",
+                    "name": "Ales Bencina"
+                },
+                "image": {
+                    "@type": "ImageObject",
+                    "@id": "https://dataliberate.com/wp-content/uploads/2019/05/Metadata_Evolution_the_Final_Mile.jpg",
+                    "url": "https://dataliberate.com/wp-content/uploads/2019/05/Metadata_Evolution_the_Final_Mile.jpg",
+                    "height": "362",
+                    "width": "388"
+                },
+                "url": "https://dataliberate.com/2019/05/14/library-metadata-evolution-final-mile/",
+                "keywords": [
+                    "rust",
+                    "programming",
+                    "software development"
+                ]
+            }
+            ]
+    }
+    </script>
 </div>

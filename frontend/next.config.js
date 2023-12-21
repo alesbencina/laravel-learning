b9913c429a71ad08ad178b4e09ce0cde40735e2d
Ales Bencina <ales.bencina@agiledrop.com>
2023-12-21 12:01:07 +0100

    rvrt

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,10 +2,7 @@
 const nextConfig = {
     images: {
         domains: ['laravel-learning.ddev.site'],
-        loader: 'akamai',
-        path: '',
     },
-    assetPrefix: './',
 }
 
 module.exports = nextConfig

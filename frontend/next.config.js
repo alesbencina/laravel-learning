1836ffa356c5549fb70533fd7c558f404ecfc282
Ales Bencina <ales.bencina@agiledrop.com>
2023-12-22 12:04:08 +0100

    aa

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
+
+const repo = 'laravel-learning'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
+
 const nextConfig = {
     images: {
         domains: ['laravel-learning.ddev.site'],
         formats: ['image/avif', 'image/webp'],
         unoptimized: true
     },
-    basePath: '/laravel-learning',
+    assetPrefix: assetPrefix,
+    basePath: basePath,
 }
 
 module.exports = nextConfig
